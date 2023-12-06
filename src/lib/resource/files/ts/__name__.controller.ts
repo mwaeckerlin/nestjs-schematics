dcc3ed723fd5eb3e9958ec292ccd66ba8b2e0bb6
Marc Wäckerlin <4056726+mwaeckerlin@users.noreply.github.com>
2023-12-06 12:01:29 +0100

    spread-analysis improved

--- src/lib/resource/files/ts/__name__.controller.ts
+++ src/lib/resource/files/ts/__name__.controller.ts
@@ -5,6 +5,8 @@ import {MessagePattern, Payload} from '@nestjs/microservices'<%
 } %>
 import {<%= classify(name) %>Service} from './<%= name %>.service'<% if (crud) { %>
 import {<%= singular(classify(name)) %>, Create<%= singular(classify(name)) %>, Update<%= singular(classify(name)) %>} from './<%= singular(name) %>.entity'<% } %>
+import {FilterQuery} from '@mikro-orm/core'
+import {ApiQuery} from '@nestjs/swagger'
 
 <% if (type === 'rest') { %>@Controller('<%= dasherize(parentname) %>/<%= dasherize(name) %>')<% } else { %>@Controller()<% } %>
 @UseInterceptors(ClassSerializerInterceptor)
@@ -18,7 +20,8 @@ export class <%= classify(name) %>Controller {
   }
 
   @Get()
-  async findAll(@Query() query?: Record<string, any>): Promise<Array<<%= singular(classify(name))%>>> {
+  @ApiQuery({name: 'query', required: false, type: 'object'})
+  async findAll(@Query('query') query?: FilterQuery<<%= singular(classify(name)) %>>): Promise<Array<<%= singular(classify(name))%>>> {
     return this.<%= lowercased(name) %>Service.findAll(query)
   }
 
@@ -28,7 +31,8 @@ export class <%= classify(name) %>Controller {
   }
 
   @Get()
-  async findOneQuery(@Query() query?: Record<string, any>): Promise<<%= singular(classify(name)) %>> {
+  @ApiQuery({name: 'query', required: false, type: 'object'})
+  async findOneQuery(@Query('query') query?: FilterQuery<<%= singular(classify(name)) %>>): Promise<<%= singular(classify(name)) %>> {
     return this.<%= lowercased(name) %>Service.findOne(query)
   }
 
@@ -38,7 +42,8 @@ export class <%= classify(name) %>Controller {
   }
 
   @Patch()
-  async updateQuery(@Body() update<%= singular(classify(name)) %>: Update<%= singular(classify(name)) %>, @Query() query?: Record<string, any>): Promise<<%= singular(classify(name)) %>> {
+  @ApiQuery({name: 'query', required: false, type: 'object'})
+  async updateQuery(@Body() update<%= singular(classify(name)) %>: Update<%= singular(classify(name)) %>, @Query('query') query?: FilterQuery<<%= singular(classify(name)) %>>): Promise<<%= singular(classify(name)) %>> {
     return this.<%= lowercased(name) %>Service.update(query, update<%= singular(classify(name)) %>)
   }
 
@@ -48,7 +53,8 @@ export class <%= classify(name) %>Controller {
   }<% } else if (type === 'microservice' && crud) { %>
 
   @Delete()
-  async removeQuery(@Query() query?: Record<string, any>): Promise<<%= singular(classify(name)) %>> {
+  @ApiQuery({name: 'query', required: false, type: 'object'})
+  async removeQuery(@Query('query') query?: FilterQuery<<%= singular(classify(name)) %>>): Promise<<%= singular(classify(name)) %>> {
     return this.<%= lowercased(name) %>Service.remove(query)
   }<% } else if (type === 'microservice' && crud) { %>
 
@@ -58,22 +64,26 @@ export class <%= classify(name) %>Controller {
   }
 
   @MessagePattern(Topic.FIND_ALL_<%= uppercased(name) %>)
-  async findAll(@Payload query?: Record<string, any>): Promise<Array<<%= singular(classify(name)) %>>> {
+  @ApiQuery({name: 'query', required: false, type: 'object'})
+  async findAll(@Payload query?: FilterQuery<<%= singular(classify(name)) %>>): Promise<Array<<%= singular(classify(name)) %>>> {
     return this.<%= lowercased(name) %>Service.findAll(query)
   }
 
   @MessagePattern(Topic.FIND_ONE_<%= singular(uppercased(name)) %>)
-  async findOne(@Payload() id: string | Record<string, any>): Promise<<%= singular(classify(name)) %>> {
+  @ApiQuery({name: 'query', required: false, type: 'object'})
+  async findOne(@Payload() id: string | FilterQuery<<%= singular(classify(name)) %>>): Promise<<%= singular(classify(name)) %>> {
     return this.<%= lowercased(name) %>Service.findOne(id)
   }
 
   @MessagePattern(Topic.UPDATE_<%= singular(uppercased(name)) %>)
-  async update(@Payload() id: string | Record<string, any>, @Payload() update<%= singular(classify(name)) %>: Update<%= singular(classify(name)) %>): Promise<<%= singular(classify(name)) %>> {
+  @ApiQuery({name: 'query', required: false, type: 'object'})
+  async update(@Payload() id: string | FilterQuery<<%= singular(classify(name)) %>>, @Payload() update<%= singular(classify(name)) %>: Update<%= singular(classify(name)) %>): Promise<<%= singular(classify(name)) %>> {
     return this.<%= lowercased(name) %>Service.update(id, update<%= singular(classify(name)) %>)
   }
 
   @MessagePattern(Topic.DELETE_<%= singular(uppercased(name)) %>')
-  async remove(@Payload() id: string | Record<string, any>): Promise<<%= singular(classify(name)) %>> {
+  @ApiQuery({name: 'query', required: false, type: 'object'})
+  async remove(@Payload() id: string | FilterQuery<<%= singular(classify(name)) %>>): Promise<<%= singular(classify(name)) %>> {
     return this.<%= lowercased(name) %>Service.remove(id)
   }<% } %>
 }
--- src/lib/resource/files/ts/__name__.service.ts
+++ src/lib/resource/files/ts/__name__.service.ts
@@ -1,6 +1,6 @@
 import {Injectable, Inject, Logger} from '@nestjs/common'
 import {ClientKafka} from '@nestjs/microservices'
-<% if (crud) { %>import {EntityManager} from '@mikro-orm/core'
+<% if (crud) { %>import {EntityManager, FilterQuery} from '@mikro-orm/core'
 import {Topic} from '@scrypt-swiss/nest'
 import {<%= singular(classify(name)) %>, Create<%= singular(classify(name)) %>, Update<%= singular(classify(name)) %>} from './<%= singular(name) %>.entity'
 <% } %>
@@ -16,15 +16,15 @@ export class <%= classify(name) %>Service {<% if (crud) { %>
     return <%= lowercased(singular(name)) %>
   }
 
-  async findAll(query: Record<string, any> = {}): Promise<<%= singular(classify(name)) %>[]> {
+  async findAll(query: FilterQuery<<%= singular(classify(name)) %>> = {}): Promise<<%= singular(classify(name)) %>[]> {
     return await this.em.find(<%= singular(classify(name)) %>, query)
   }
 
-  async findOne(id: string | Record<string, any> = {}): Promise<<%= singular(classify(name)) %>> {
+  async findOne(id: string | FilterQuery<<%= singular(classify(name)) %>> = {}): Promise<<%= singular(classify(name)) %>> {
     return this.em.findOneOrFail(<%= singular(classify(name)) %>, id)
   }
 
-  async update(id: string | Record<string, any> = {}, <% if (type !== 'graphql-code-first' && type !== 'graphql-schema-first') { %>update<%= singular(classify(name)) %>: Update<%= singular(classify(name)) %><% } else { %>update<%= singular(classify(name)) %>Input: Update<%= singular(classify(name)) %>Input<% } %>): Promise<<%= singular(classify(name)) %>> {
+  async update(id: string | FilterQuery<<%= singular(classify(name)) %>> = {}, <% if (type !== 'graphql-code-first' && type !== 'graphql-schema-first') { %>update<%= singular(classify(name)) %>: Update<%= singular(classify(name)) %><% } else { %>update<%= singular(classify(name)) %>Input: Update<%= singular(classify(name)) %>Input<% } %>): Promise<<%= singular(classify(name)) %>> {
     return await this.em.transactional(async (em) => {
       const <%= lowercased(singular(name)) %> = await em.findOneOrFail(<%= singular(classify(name)) %>, id)
       Object.assign(<%= lowercased(singular(name)) %>, update<%= singular(classify(name)) %>, {merge: true})
@@ -34,7 +34,7 @@ export class <%= classify(name) %>Service {<% if (crud) { %>
     })
   }
 
-  async remove(id: string | Record<string, any> = {}): Promise<<%= singular(classify(name)) %>> {
+  async remove(id: string | FilterQuery<<%= singular(classify(name)) %>> = {}): Promise<<%= singular(classify(name)) %>> {
     const <%= lowercased(singular(name)) %> = await this.em.findOneOrFail(<%= singular(classify(name)) %>, id)
     await this.em.removeAndFlush(<%= lowercased(singular(name)) %>)
     this.kafka.emit(Topic.<%= uppercased(singular(name)) %>_DELETED, <%= lowercased(singular(name)) %>)
