9aa0125e84df7be541698aa9fbd365d819e1ca23
Marc Wäckerlin <4056726+mwaeckerlin@users.noreply.github.com>
2024-12-04 09:39:28 +0100

    review comments

--- src/lib/controller/files/ts/__name__.controller.ts
+++ src/lib/controller/files/ts/__name__.controller.ts
@@ -1,9 +1,10 @@
-import {Controller, UseInterceptors, ClassSerializerInterceptor, UsePipes, ValidationPipe, Logger} from '@nestjs/common'
+import {Controller, Logger} from '@nestjs/common'
+import {UseClassSerializer, UseValidation} from '@scrypt-swiss/nest'
 import {<%= classify(name) %>Service} from './<%= dasherize(name) %>.service'
 
 @Controller('<%= dasherize(project) %>/<%= dasherize(name) %>')
-@UsePipes(new ValidationPipe({transform: true}))
-@UseInterceptors(ClassSerializerInterceptor)
+@UseValidation()
+@UseClassSerializer()
 export class <%= classify(name) %>Controller {
   readonly logger = new Logger(this.constructor.name)
   constructor(private readonly <%= lowercased(name) %>: <%= classify(name) %>Service) {}
